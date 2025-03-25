cd91d200bdaad602aa98e9f98f5752a90a09b7c0
dadadadididada <yipyeeep@gmail.com>
2025-03-25 14:30:17 +0800

    feat: add routing for major components

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import { createContext } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
+import MessageBoard from './MessageBoard';
+import AllPosts from './AllPost';
+import PostView from './PostView';
+import Welcome from './Welcome';
+import NavBar from './NavBar';
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <MessageBoard />,
+        children: [
+          {
+            path: ":pageNumber",
+            element: <AllPosts />,
+          },
+          {
+            path: "post/:postId",
+            element: <PostView />,
+          },
+        ],
+      },
+      {
+        path: "welcome",
+        element: <Welcome />,
+        //loader: welcomeLoader,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
 }
 
 export default App
+
+function Layout() {
+  return (
+    <>
+      <NavBar />
+      <Outlet />
+    </>
+  );
+}
