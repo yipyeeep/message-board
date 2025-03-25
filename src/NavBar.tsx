665a5e9d206e30356889f02f4bddfff085c04bb8
dadadadididada <yipyeeep@gmail.com>
2025-03-25 19:06:20 +0800

    refactor: change to cat logo

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,14 +1,15 @@
 import { Link } from "react-router-dom";
+import logo from "../src/assets/logoipsum-338.svg"
 
 export default function NavBar() {
   return (
     <>
       <nav className="nav-bar">
-        <Link className="nav-logo-link" to="/">
+        <Link className="nav-logo-link w-32 h-auto" to="/">
           <img
             id="logo"
             className="nav-logo"
-            src="https://supaship.io/supaship_logo_with_text.svg"
+            src={logo}
             alt="logo"
           />
         </Link>
--- supabase/migrations/20250325051722_original-ddl.sql
+++ supabase/migrations/20250325051722_original-ddl.sql
@@ -1,6 +1,6 @@
-create_table user_profiles (
-    user_id uuid primary key references auth.users (id) not null,
-    username text unique not null
+CREATE TABLE IF NOT EXISTS user_profiles (
+    user_id uuid PRIMARY KEY REFERENCES auth.users (id) NOT NULL,
+    username TEXT UNIQUE NOT NULL
     CONSTRAINT proper_username CHECK (username ~* '^[a-zA-Z0-9_]+$')
     CONSTRAINT username_length CHECK (char_length(username) > 3 and char_length(username) < 15)
 );
