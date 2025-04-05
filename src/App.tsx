b0c2a43207a1cec6771ce7b5f7e3f0b3f0b78f45
dadadadididada <yipyeeep@gmail.com>
2025-04-05 16:09:46 +0800

    feat: add context for accessing user info

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,38 @@
 //import { createContext } from 'react'
 import './App.css'
 
+import { createContext } from "react";
 import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
 import MessageBoard from './MessageBoard';
 import AllPosts from './AllPost';
 import PostView from './PostView';
 import Welcome from './Welcome';
 import NavBar from './NavBar';
-
+import { SupashipUserInfo, useSession } from "./use-session";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "",
-        element: <MessageBoard />,
-        children: [
-          {
-            path: ":pageNumber",
-            element: <AllPosts />,
-          },
-          {
-            path: "post/:postId",
-            element: <PostView />,
-          },
-        ],
-      },
-      {
-        path: "welcome",
-        element: <Welcome />,
-        //loader: welcomeLoader,
-      },
-    ],
-  },
+  /* routing hasn't changed */
 ]);
 
 function App() {
   return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
+
+export const UserContext = createContext<SupashipUserInfo>({
+  session: null,
+  profile: null,
+});
 
 function Layout() {
+  const supashipUserInfo = useSession();
   return (
     <>
-      <NavBar />
-      <Outlet />
+      <UserContext.Provider value={supashipUserInfo}>
+        <NavBar />
+        <Outlet />
+      </UserContext.Provider>
     </>
   );
 }
