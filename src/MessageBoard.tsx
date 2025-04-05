c174f55307b3e1131ef048d70b4e2e698eca258e
dadadadididada <yipyeeep@gmail.com>
2025-04-05 16:53:38 +0800

    feat: add all login-components

--- src/MessageBoard.tsx
+++ src/MessageBoard.tsx
@@ -1,12 +1,26 @@
+import { useContext } from "react";
 import { Link, Outlet } from "react-router-dom";
+import { UserContext } from "./App";
+import Login from "./Login";
 
 export default function MessageBoard() {
-    return (
-      <div className="message-board-container">
-        <Link to="/1">
-          <h2 className="message-board-header-link">Message Board</h2>
-        </Link>
-        <Outlet />
-      </div>
-    );
-  }
+  const userProfile = useContext(UserContext);
+  return (
+    <div className="message-board-container">
+      <Link to="/1">
+        <h2 className="message-board-header-link">Message Board</h2>
+      </Link>
+      {userProfile.session ? (
+        <></>
+      ) : (
+        <h2
+          className="message-board-login-message"
+          data-e2e="message-board-login"
+        >
+          Yo Dawg. you gotta <Login /> to join in the discussion.
+        </h2>
+      )}
+      <Outlet />
+    </div>
+  );
+}
--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,11 +1,16 @@
 import { Link } from "react-router-dom";
 import logo from "../src/assets/logoipsum-338.svg"
+import { useContext } from "react";
+import { UserContext } from "./App";
+import Login from "./Login";
+import UserMenu from "./UserMenu";
 
 export default function NavBar() {
+  const { session } = useContext(UserContext);
   return (
     <>
       <nav className="nav-bar">
-        <Link className="nav-logo-link w-32 h-auto" to="/">
+        <Link className="nav-logo-link" to="/">
           <img
             id="logo"
             className="nav-logo"
@@ -19,6 +24,10 @@ export default function NavBar() {
             <Link to="/1" className="nav-message-board-link">
               message board
             </Link>
+          </li>
+          <li className="nav-auth-item">
+            {session?.user ? <UserMenu /> : <Login />} // see a bit further down
+            for the UserMenu.tsx file!
           </li>
         </ul>
       </nav>
